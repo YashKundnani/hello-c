7e996dc7ac14ee4a959f70312e5791b19834ea14
YashKundnani <43103704+YashKundnani@users.noreply.github.com>
2018-12-19 12:16:12 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a;
+    a=(2*n)-1;
+    printf("a=%d\n",a);
+    printf("______________________________\n");
+    int i,j,k;
+    for(i=1;i<=a;i++)
+    {
+        for(j=a;j>=1;j--)
+        {
+        for(k=n;k>=1;k--)
+        printf("%d ",n);
+            
+        }
+        
+        printf("\n");
+    }
+    
+
+    
+}
